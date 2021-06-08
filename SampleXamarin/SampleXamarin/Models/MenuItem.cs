e145b4df4c6ed6795c214cfd6eb72d9da85de3b6
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-06-08 10:17:46 +0700

    menambahkan master page

---
+++ SampleXamarin/SampleXamarin/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleXamarin.Models
+{
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+            TargetType = typeof(MenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+        public string ImageIcon { get; set; }
+    }
+}
